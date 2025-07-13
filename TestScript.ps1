407847c5c85dace45a151f016d6b3fd8b7e6c31f
Jeetendra Prasad <jeetendraprasad@users.noreply.github.com>
2025-07-13 14:18:24 +0530

    more tests added. this is not working

--- TestScript.ps1
+++ TestScript.ps1
@@ -3,3 +3,5 @@ Invoke-WebRequest -Uri 'http://localhost:3001/register' `
 	-Method POST `
 	-Body (@{ email='monster@gmail.com'; password='12345678' } | ConvertTo-Json) `
 	-ContentType 'application/json'
+	
+Invoke-WebRequest -Uri 'http://localhost:3001/posts'
